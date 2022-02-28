ef7fdbd0947de0465ff1171b88af77af4fa08f43
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-02-28 16:31:12 +0700

    sudah bisa ambil data dari api

---
+++ components/Results.tsx
@@ -0,0 +1,11 @@
+function Results({ results }: any) {
+  return (
+    <div>
+      {results.results.map((result: any) => (
+        <h1>Hello</h1>
+      ))}
+    </div>
+  )
+}
+
+export default Results
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ results }: any) => {
   return (
     <div>
       <Head>
@@ -11,12 +14,25 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-
-      {/* Nav */}
-
-      {/* Results */}
+      <Nav />
+      <Results results={results} />
     </div>
   )
 }
 
 export default Home
+
+export async function getServerSideProps(context: any) {
+  const genre = context.query.genre
+  const req = await fetch(
+    `https://api.themoviedb.org/3${
+      // @ts-ignore
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json())
+  return {
+    props: {
+      results: req,
+    },
+  }
+}
